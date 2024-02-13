cc7e9ad293c609ff02424064acf3e264c8e3dcfb
bulendra <72643390+bulendra@users.noreply.github.com>
2024-02-13 06:44:00 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-import Nav from './component/nav';
+
 import Main from './component/hero-section';
 import Section from './component/section';
 
--- src/component/nav.jsx
+++ src/component/nav.jsx
@@ -10,17 +10,17 @@ function Nav(){
                     <h1 className="font-bold text-xl">OpenAI</h1>
                 </div>
                 <div className="links  hidden md:flex gap-9">
-                    <a href="" className="hover:underline">Research</a>
-                    <a href="" className="hover:underline">API</a>
-                    <a href="" className="hover:underline">ChatGPT</a>
-                    <a href="" className="hover:underline">Saftey</a>
-                    <a href="" className="hover:underline">Company</a></div>
+                    <a href="https://youtu.be/g7_Zv5dWPcI?si=Rsh8SGb2Gp8EWOjD" className="hover:underline">Research</a>
+                    <a href="https://youtu.be/g7_Zv5dWPcI?si=Rsh8SGb2Gp8EWOjD" className="hover:underline">API</a>
+                    <a href="https://youtu.be/g7_Zv5dWPcI?si=Rsh8SGb2Gp8EWOjD" className="hover:underline">ChatGPT</a>
+                    <a href="https://youtu.be/g7_Zv5dWPcI?si=Rsh8SGb2Gp8EWOjD" className="hover:underline">Saftey</a>
+                    <a href="https://youtu.be/g7_Zv5dWPcI?si=Rsh8SGb2Gp8EWOjD" className="hover:underline">Company</a></div>
             </div>
-            <div className="menu md:hidden"><a href="">Menu</a></div>
+            <div className="menu md:hidden"><a href="https://youtu.be/g7_Zv5dWPcI?si=Rsh8SGb2Gp8EWOjD">Menu</a></div>
             <div className="right-nav hidden md:flex gap-9">
-                <a href="" className="hover:underline">Search</a>
-                <a href="" className="hover:underline">Log in</a>
-                <a href="" className="border-[1px] rounded-sm hover:bg-white hover:text-black px-2"><button>Try ChatGPT</button></a>
+                <a href="https://youtu.be/g7_Zv5dWPcI?si=Rsh8SGb2Gp8EWOjD" className="hover:underline">Search</a>
+                <a href="https://youtu.be/g7_Zv5dWPcI?si=Rsh8SGb2Gp8EWOjD" className="hover:underline">Log in</a>
+                <a href="https://youtu.be/g7_Zv5dWPcI?si=Rsh8SGb2Gp8EWOjD" className="border-[1px] rounded-sm hover:bg-white hover:text-black px-2"><button>Try ChatGPT</button></a>
             </div>
         </div>
 
